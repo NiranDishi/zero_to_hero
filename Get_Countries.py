a8bb5da592f1dc6d802d2d098530d1f0ded70902
Niran Dishi <niranile@gmail.com>
2018-12-08 15:57:02 +0200

    Hayde!

---
+++ Get_Countries.py
@@ -0,0 +1,25 @@
+########################################
+############# NIRAN'S CODE #############
+########################################
+
+
+f= open("Susna.txt","w+")
+## w+ the plus means if there wasnt any exist file Susna, then he will open a new one.
+##
+
+for i in range(10):
+     f.write("This is line %d\r\n" % (i+1)) ## \r takes the cursor to the begining of line and print over there what inside the \r in that case\n.
+
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
