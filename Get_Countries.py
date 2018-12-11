9277e624d8c57e2583e5896d8b627a56b18e6121
Niran Dishi <niranile@gmail.com>
2018-12-11 18:00:43 +0200

    task 2.1

--- Get_Countries.py
+++ Get_Countries.py
@@ -24,13 +24,17 @@ URL = "https://restcountries.eu/rest/v2/all"
 r = requests.get(url=URL) ## r get the data inside him
 # extracting data in json format
 data = r.json()
-SUSKI=open("SUSKI.txt","w+")
+A2C=open("alpha2codes.txt","w+")
 
 
 
+for i in range(len(data)): ## Going trough all the data list's length
+    A2C = open("alpha2codes.txt", "a+" )  ## open alpha2codes file with an option to append a new values
+    Dict=data[i]      ## Dict is contain the dictionery in data[i]'s list.
+    Val=Dict.get("alpha2Code") ## Val is containing the value inside alpha2Coda's key.
+    A2C.write("%s\r\n" % Val)  ##add to SUSKI file a new line with the alpha2Code's value
+    A2C.close() ## close the file
 
-
-print(data)
 
 
 
